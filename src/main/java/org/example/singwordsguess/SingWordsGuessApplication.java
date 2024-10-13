afbb9d69f3091285eef6bec51bfa24a3e6c7076b
Victoryline <victoryline629@naver.com>
2024-10-13 13:46:07 +0900

    first commit

---
+++ src/main/java/org/example/singwordsguess/SingWordsGuessApplication.java
@@ -0,0 +1,13 @@
+package org.example.singwordsguess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingWordsGuessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingWordsGuessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/singwordsguess/SingWordsGuessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.singwordsguess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingWordsGuessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
